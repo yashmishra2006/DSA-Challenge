67964a3d692b707eebc16312604082577986e483
arc-wonders <arkinkansra@gmail.com>
2026-01-01 23:51:38 +0530

    largest in array

--- Arkin/easy/largest_element.cpp
+++ Arkin/easy/largest_element.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
 using namespace std; 
 
-int largest_element(int arr[]){
+int largest_element(int arr[], int n){
+    int max = arr[0]; 
+    for (int i = 0; i < n; i++)
+    {
+        if (max < arr[i]){
+            max = arr[i]; 
+        }
+    }
+    return max; 
 }
 
 
@@ -10,6 +18,11 @@ int main(){
     cin>>n; 
     int arr[n]; 
 
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+
+    cout<<largest_element(arr, n); 
     
     return 0;
 }
