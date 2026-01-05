bc1498fa3b422d6ef5fbddbf9ebfda509aa9bd34
Amank07-debug <idforaman2019@gmail.com>
2026-01-05 20:56:04 +0530

    DAY0005_05/01/26_Solved#1838

---
+++ Aman/Easy/1838_FrequencyofElement.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int maxFrequency(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        long sum = 0;
+        int l = 0, maxFreq = 0;
+
+        for (int r = 0; r < nums.length; r++) {
+            sum += nums[r];
+
+            while ((long) nums[r] * (r - l + 1) - sum > k) {
+                sum -= nums[l];
+                l++;
+            }
+
+            maxFreq = Math.max(maxFreq, r - l + 1);
+        }
+
+        return maxFreq;
+    }
+}
